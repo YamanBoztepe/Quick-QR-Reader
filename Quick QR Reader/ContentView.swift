5a37fccded95ff18c7dad0b4cc199564c57e0d0a
Yaman Boztepe <yaman.boztepe@gmail.com>
2021-10-09 21:25:49 +0300

    Initial Commit

---
+++ Quick QR Reader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quick QR Reader
+//
+//  Created by Yaman Boztepe on 9.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quick QR Reader/Quick_QR_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_QR_ReaderApp.swift
+//  Quick QR Reader
+//
+//  Created by Yaman Boztepe on 9.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct Quick_QR_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
